4c1316edda5c1518674bd3dacd3bf77171c6961b
jiajia <3212946755@qq.com>
2017-07-24 22:23:49 +0800

    register

--- express_modules/app.js
+++ express_modules/app.js
@@ -1,31 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+// var path = require('path')
+// var favicon = require('serve-favicon')
+// var logger = require('morgan')
+// var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-// var index = require('./routes/index');
-// var users = require('./routes/users');
+var MongoClient = require('mongodb').MongoClient
+var DB_CONN_STR = 'mongodb://localhost:27017/Collections'
+var data
 
-var app = express();
+var insertData = function(db, data, callback) {  
+    //连接到表 example
+    var collection = db.collection('example')
+    //插入数据
+    collection.insert(data, function (err, result) { 
+        if(err)
+        {
+            console.log('Error:'+ err)
+            return
+        }     
+        callback(result)
+    })
+}
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/public', index);
-app.get('/register', function(req, res, next) {
-  res.send('respond with a resource');
-});
+app.post('/register', function(req, res, next) { 
+  MongoClient.connect(DB_CONN_STR, function(err, db) {
+      console.log("连接成功！")
+      insertData(db, req.body, function (result) {
+          console.log(result)
+          db.close()
+      })
+  })
+  console.log(req.body)
+  res.json({result: 0})
+})
 
 // app.use('/users', users);
 app.get('/public/*', function(req, res, next) {
@@ -39,18 +50,18 @@ app.get('/public/*', function(req, res, next) {
         }
     };
     // 由于拿到的数据是个数组（前面用了*匹配），从index.html开始，所以filename取第一个
-    var fileName = req.params[0];
+    var fileName = req.params[0]
     // 通过sendFile()函数取到主页面的内容并展现出来
     res.sendFile(fileName, options, function(err) {
         if (err) {
             console.log(err);
-            res.status(err.status).end();
+            res.status(err.status).end()
         }
         else {
-            console.log('sent', fileName);
+            console.log('sent', fileName)
         }
-    });
-});
+    })
+})
 app.listen(3000, function () {
   console.log('Open successfully!')
-});
+})
--- express_modules/public/js/index.js
+++ express_modules/public/js/index.js
@@ -1,18 +1,83 @@
+// 登录注册链接
 var barRightRegister = $('.bar-right-register')[0]
+var barRightLogin = $('.bar-right-login')[0]
+var infoClose = $('.info-title-close')[0]
+var infoCover = $('.info-cover')[0]
 
-
-
+// 跳转登录界面
+var jumpLogin = function () {
+  $('.info-board')[0].style.display = 'block'
+  $('.info-title-word').html('登录')
+  $('.info-register')[0].style.display = 'none'
+  $('.info-login')[0].style.display = 'block'
+  $('.info-message-register')[0].style.display = 'none'
+  $('.info-message-login')[0].style.display = 'block'
+}
+// 跳转注册界面
+var jumpRegister = function () {
+  $('.info-board')[0].style.display = 'block'
+  $('.info-title-word').html('注册')
+  $('.info-register')[0].style.display = 'block'
+  $('.info-login')[0].style.display = 'none'
+  $('.info-message-register')[0].style.display = 'block'
+  $('.info-message-login')[0].style.display = 'none'
+}
+// 点击注册链接
 barRightRegister.addEventListener('click', function () {
-  $('.info-cover').style.display = 'block'
-  $('.info-board').style.dispaly = 'block'
+  $('.info-cover')[0].style.display = 'block'
+  jumpRegister()
 })
-
+$('.info-message-login a')[0].addEventListener('click', function () {
+  jumpRegister()
+})
+// 点击登录链接
+barRightLogin.addEventListener('click', function () {
+  $('.info-cover')[0].style.display = 'block'
+  jumpLogin()
+})
+$('.info-message-register a')[0].addEventListener('click', function () {
+  jumpLogin()
+})
+// 清除未填完信息
+var infoClear = function () {
+  $('.info-cover')[0].style.display = 'none'
+  $('.info-board')[0].style.display = 'none'
+  $('.info-register input').val('')
+  $('.info-login input').val('')
+}
+// 点击叉号
+infoClose.addEventListener('click', function () {
+  infoClear()
+})
+// 点击登录注册页面的背景
+infoCover.addEventListener('click', function (e) {
+  if (e.target.className === 'info-cover') {
+    infoClear()
+  }
+})
+// 验证邮箱函数
+var checkMail = function(email){ 
+  var reg = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
+  var res = reg.test(email);
+  return res; 
+} 
 $('.info-submit')[0].addEventListener('click', function () {
+  if (register.rpassword.value !== register.rconfirm.value) {
+    alert('两次密码不一样！')
+    return false
+  } else if (register.rusername.value === '' || register.rpassword.value === '' || register.remail.value === '') {
+    alert('信息没有填写完整！')
+    return false
+  }
+  if (!checkMail(register.remail.value)) {
+    alert('邮箱错误，请重填！')
+    return false
+  }
   $.ajax({
     type: 'POST',
-    url: 'http://localhost/register',
+    url: '/register',
     dataType: 'json',
-    'Content-Type': 'application/json;charset=utf-8',
+    'contentType': 'application/json',
     data: JSON.stringify(
       {
         'username': register.rusername.value,
@@ -23,6 +88,7 @@ $('.info-submit')[0].addEventListener('click', function () {
     success: function (data) {
       if (data.result === 0) {
         alert('成功')
+        infoClear()
       } else if (data.result === 1) {
         alert('')
       }
